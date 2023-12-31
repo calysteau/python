0f0f92bc0872471a3dc88424620d2788f1dfdf8e
Justeau Yann <calysteau@outlook.fr>
2023-12-31 16:17:02 +0100

    Clean code

--- TIFtoCOG/TIFtoCOG.py
+++ TIFtoCOG/TIFtoCOG.py
@@ -3,51 +3,53 @@ import os
 from tqdm import tqdm
 import rasterio
 from rasterio.shutil import copy
-from rasterio.enums import Resampling
+import copy as python_copy  # Import the copy method from the built-in copy module
 
-# Créer un analyseur d'arguments
-parser = argparse.ArgumentParser(description='Convertir des fichiers GeoTIFF en Cloud Optimized GeoTIFF (COG).')
-parser.add_argument('--repertoire_geotiff', required=True, help='Répertoire contenant les fichiers GeoTIFF à convertir.')
-parser.add_argument('--repertoire_cog', required=True, help='Répertoire de sortie pour les fichiers COG convertis.')
+# Create an argument parser
+parser = argparse.ArgumentParser(description='Convert GeoTIFF files to Cloud Optimized GeoTIFF (COG).')
+parser.add_argument('--input_geotiff_dir', required=True, help='Directory containing GeoTIFF files to convert.')
+parser.add_argument('--output_cog_dir', required=True, help='Output directory for the converted COG files.')
 args = parser.parse_args()
 
-# Répertoires pour les fichiers GeoTIFF et COG
-repertoire_geotiff = args.repertoire_geotiff
-repertoire_cog = args.repertoire_cog
+# Directories for GeoTIFF and COG files
+input_geotiff_dir = args.input_geotiff_dir
+output_cog_dir = args.output_cog_dir
 
-# Créer le répertoire de sortie pour les COG
-if not os.path.exists(repertoire_cog):
-    os.makedirs(repertoire_cog)
+# Create output directory for COGs
+if not os.path.exists(output_cog_dir):
+    os.makedirs(output_cog_dir)
 
-# Fonction pour convertir un fichier GeoTIFF en COG
-def convertir_geotiff_en_cog(chemin_fichier_geotiff, repertoire_cog):
-    with rasterio.open(chemin_fichier_geotiff) as src:
-        profile = src.profile.copy()
+
+# Function to convert a GeoTIFF file to COG
+def convert_geotiff_to_cog(geotiff_path, cog_dir):
+    with rasterio.open(geotiff_path) as src:
+        profile = python_copy.copy(src.profile)  # Use the copy method to copy the profile
         profile.update(driver='GTiff', tiled=True, blockxsize=512, blockysize=512, compress='LZW')
 
-        chemin_relatif = os.path.relpath(chemin_fichier_geotiff, repertoire_geotiff)
-        chemin_sans_extension, _ = os.path.splitext(chemin_relatif)
-        chemin_fichier_cog = os.path.join(repertoire_cog, chemin_sans_extension + ".tif")
+        relative_path = os.path.relpath(geotiff_path, input_geotiff_dir)
+        path_without_extension, _ = os.path.splitext(relative_path)
+        cog_file_path = os.path.join(cog_dir, path_without_extension + ".tif")
 
-        os.makedirs(os.path.dirname(chemin_fichier_cog), exist_ok=True)
+        os.makedirs(os.path.dirname(cog_file_path), exist_ok=True)
 
-        copy(src, chemin_fichier_cog, copy_src_overviews=True, **profile)
+        copy(src, cog_file_path, copy_src_overviews=True, **profile)
 
-# Compter le nombre total de fichiers GeoTIFF
-total_fichiers_geotiff = sum(1 for _, _, fichiers in os.walk(repertoire_geotiff) for fichier in fichiers if fichier.endswith(".tif"))
 
-# Barre de progression
-barre_de_progression = tqdm(total=total_fichiers_geotiff, unit="fichier")
+# Count total number of GeoTIFF files
+total_geotiff_files = sum(1 for _, _, files in os.walk(input_geotiff_dir) for file in files if file.endswith(".tif"))
 
-# Parcourir et convertir les fichiers GeoTIFF en COG
-for dossier, _, fichiers in os.walk(repertoire_geotiff):
-    for fichier in fichiers:
-        if fichier.endswith(".tif"):
-            chemin_fichier_geotiff = os.path.join(dossier, fichier)
-            convertir_geotiff_en_cog(chemin_fichier_geotiff, repertoire_cog)
-            barre_de_progression.update(1)
+# Progress bar
+progress_bar = tqdm(total=total_geotiff_files, unit="file")
 
-# Fermer la barre de progression
-barre_de_progression.close()
+# Loop through and convert GeoTIFF files to COG
+for folder, _, files in os.walk(input_geotiff_dir):
+    for file in files:
+        if file.endswith(".tif"):
+            geotiff_file_path = os.path.join(folder, file)
+            convert_geotiff_to_cog(geotiff_file_path, output_cog_dir)
+            progress_bar.update(1)
 
-print("Conversion en COG terminée.")
+# Close the progress bar
+progress_bar.close()
+
+print("COG conversion completed.")
